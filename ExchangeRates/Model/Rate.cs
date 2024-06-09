ba875f0167aab6c3028cca8e6c2ec2323a42cabd
Vasiliy <algoritmick.vm@gmail.com>
2024-06-09 22:48:52 +0300

    init porject

---
+++ ExchangeRates/Model/Rate.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ExchangeRates.Model
+{
+    /// <summary>
+    /// класс Валюты
+    /// </summary>
+    public class Rates : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// код валюты
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// наименование валюты
+        /// </summary>
+        private string nameRates;
+        public string NameRates
+        {
+            get { return nameRates; }
+            set
+            {
+                nameRates = value;
+                OnPropertyChanged("NameRates");
+
+            }
+        }
+
+        public Rates() { }
+        public Rates(int id, string nameRates)
+        {
+            this.Id = id;
+            this.NameRates = nameRates;
+        }
+
+        /// <summary>
+        /// Метод поверхностного копирования 
+        /// </summary>
+        /// <returns></returns>
+        public Rates ShallowCopy()
+        {
+            return (Rates)this.MemberwiseClone();
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+         [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName]
+        string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+
+    
+}
---
+++ ExchangeRates/ViewModel/RatesViewModel.cs
@@ -0,0 +1,63 @@
+﻿using ExchangeRates.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRates.ViewModel
+{
+    public class RatesViewModel : INotifyPropertyChanged
+    {
+
+        /// <summary>
+        /// выбранная в списке валюта
+        /// </summary>
+        private Rate selectedRate;
+       
+        public Rate SelectedRate
+        {
+            get
+            {
+                return selectedRate;
+            }
+            set
+            {
+                selectedRole = value;
+                OnPropertyChanged("SelectedRate");
+              //  EditRole.CanExecute(true);
+            }
+        }
+
+        /// <summary>
+        /// коллекция Доступных Валют
+        /// </summary>
+        public ObservableCollection<Rate> ListRates { get; set; } = new
+       ObservableCollection<Rate>();
+
+        public RatesViewModel()
+        {
+            this.ListRates.Add(new Rate
+            {
+                Id = 1,
+                NameRate = "Доллар",
+                Code= "USD"
+            });
+            this.ListRates.Add(new Rate
+            {
+                Id = 2,
+                NameRate = "Евро",
+                Code = "Eur"
+            });
+            this.ListRates.Add(new Rate
+            {
+                Id = 3,
+                NameRate = "Юань",
+                Code = "CNY"
+            });
+        }
+    }
+}
